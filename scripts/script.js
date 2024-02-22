cfe4cc2dfc77c128cc9a6b0697bde8726de8d1b9
julien-vaz <jhmvaz95@gmail.com>
2024-02-22 15:45:59 -0300

    Função addNewTask() implementada

--- scripts/script.js
+++ scripts/script.js
@@ -17,3 +17,45 @@ function markAsCompleted(checkbox) {
         listItem.classList.remove("completed");
     };
 };
+
+/*
+Função que adiciona uma nova tarefa na lista,
+verificando antes se já existe uma tarefa com o mesmo nome.
+*/
+function addNewTask() {
+    const taskList = document.getElementById("taskList");
+    console.log(taskList);
+    const newTaskText = document.getElementById("newItemText").value;
+
+    // Verifica se a tarefa possui um nome.
+    if (newTaskText === "") {
+        window.alert("Escolha um nome para a tarefa.");
+
+    // Verifica se a tarefa já existe na lista.
+    } else if (Array.from(taskList.querySelectorAll("li"))
+        .find((task) => task.querySelector("p").innerText == newTaskText) != undefined) {
+        window.alert("Essa tarefa já foi adicionada.");
+    } else {
+        
+        // Cria um item e altera o HTML interno
+        const newTask = document.createElement("li");
+        newTask.innerHTML = `<p>${newTaskText}</p>`;
+
+        // Cria uma checkbox
+        const checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.onchange = () => { markAsCompleted(checkbox) };
+        newTask.appendChild(checkbox);
+
+        // Cria um botão customizado
+        const button = document.createElement("button");
+        button.innerHTML = "&#x2716;";
+        button.classList.add("delete");
+        newTask.appendChild(button);
+
+        // Adiciona o novo item à lista
+        newTask.classList.add("task");
+        taskList.appendChild(newTask);
+    }
+};
+
